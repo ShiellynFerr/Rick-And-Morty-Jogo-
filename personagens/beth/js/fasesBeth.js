5b187a523111eba0d1acb39d705f68039c7278cc
Shiellyn <brunashiellyn@gmail.com>
2022-05-02 16:27:54 -0300

    adicionado funções das fases da personagem beth

---
+++ personagens/beth/js/fasesBeth.js
@@ -0,0 +1,70 @@
+
+function fase1e2Beth() {
+
+    let escolha1;
+
+    while(escolha1 !== 'a' && escolha1 !== 'b' && escolha1 !== 'c') {
+  
+      escolha1 = prompt('Escolha uma alternativa!');
+  
+      if(escolha1 === 'c') {
+          location.href = '../beth/bethFase2.html';
+          alert('Muito bem, você avançou para a próxima fase!');
+      } else if (escolha1 === 'a') {
+        location.href = '../beth/bethGameOver.html';
+      } else if (escolha1 === 'b') {
+        location.href = '../beth/bethGameOver.html';
+      }else {
+          alert('Escolha Inválida, escolha entre a b ou c');
+      }
+  
+    }
+
+}
+
+
+function fase2Beth() {
+
+    let escolha1;
+
+    while(escolha1 !== 'a' && escolha1 !== 'b' && escolha1 !== 'c') {
+  
+      escolha1 = prompt('Escolha uma alternativa!');
+  
+      if(escolha1 === 'c') {
+        location.href = '../beth/bethFase3.html';
+          alert('Muito bem, você avançou para a próxima fase!');
+      } else if (escolha1 === 'a') {
+        location.href = '../beth/bethGameOver.html';
+      } else if (escolha1 === 'b') {
+        location.href = '../beth/bethGameOver.html';
+      }else {
+          alert('Escolha Inválida, escolha entre a b ou c');
+      }
+  
+    }
+
+}
+
+
+function fase3Beth() {
+
+    let escolha1;
+
+    while(escolha1 !== 'a' && escolha1 !== 'b' && escolha1 !== 'c') {
+  
+      escolha1 = prompt('Escolha uma alternativa!');
+  
+      if(escolha1 === 'a') {
+        location.href = '../beth/bethGanhou.html';
+      } else if (escolha1 === 'a') {
+        location.href = '../beth/bethGameOver.html';
+      } else if (escolha1 === 'c') {
+        location.href = '../beth/bethGameOver.html';
+      }else {
+          alert('Escolha Inválida, escolha entre a b ou c');
+      }
+  
+    }
+
+}
