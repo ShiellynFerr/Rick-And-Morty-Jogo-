3f54201297fb3af6e4f96240878026e559e2300f
Shiellyn <brunashiellyn@gmail.com>
2022-05-02 16:08:06 -0300

    adicionando funções que redirecionam para a primeira fase de cada personagem

---
+++ js/escolhaPersonagem.js
@@ -0,0 +1,15 @@
+function morty() {
+      
+    location.href = '../personagens/morty/mortyFase1.html';
+}
+
+function rick() {
+      
+    location.href = '../personagens/rick/rickFase1.html';
+}
+
+
+function beth() {
+      
+    location.href = '../personagens/beth/bethFase1.html';
+}
